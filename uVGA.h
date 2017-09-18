fe31b096d3cc5b5d8bd3365347a2da4930ca534b
Frank <FrankBoesing@users.noreply.github.com>
2017-09-18 22:03:19 +0200

    Update uVGA.h

--- uVGA.h
+++ uVGA.h
@@ -132,11 +132,11 @@ public:
 	// =========================================================
 	// video settings
 	// =========================================================
-	// Default: DMA0, DMA1, DMA2, HSYNC on FTM0_CH0 (uses FTM0_CH0, CH1, CH7)
+	// Default: HSYNC on FTM0_CH0 (uses FTM0_CH0, CH1, CH7)
 	//          VSYNC on pin 29 (teensy 3.5, 3.6), 10 (teensy 3.2)
 	//          gfx_dma = last dma channel available
 	// =========================================================
-	uVGA(int dma_number = 0, int sram_u_dma_number = 1, int sram_u_dma_fix_number = 2, int hsync_ftm_num = 0, int hsync_ftm_channel_num = 0, int x1_ftm_channel_num = 7,int vsync_pin = DEFAULT_VSYNC_PIN, int graphic_dma = DMA_NUM_CHANNELS - 1);
+	uVGA(int dma_number = 0, int sram_u_dma_number = 0, int sram_u_dma_fix_number = 0, int hsync_ftm_num = 0, int hsync_ftm_channel_num = 0, int x1_ftm_channel_num = 7,int vsync_pin = DEFAULT_VSYNC_PIN, int graphic_dma = DMA_NUM_CHANNELS - 1);
 
 	// display VGA image
 	uvga_error_t begin(uVGAmodeline *modeline = NULL);
